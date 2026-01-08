a60839b05c9a6a6c8e2ec372d2dea973ebc8ce6e
dflindt <df@dflindtatflindt.com>
2026-01-08 12:58:55 +0100

    can show equalized hostogram

--- filters.py
+++ filters.py
@@ -5,18 +5,57 @@ from matplotlib import pyplot as plot
 imgResizeX = 500
 imgResizeY = 400
 
-grayscale = cv2.imread("images/hylstre_maatte.jpg", 0) #Convert to grayscale
+img_original = cv2.imread("images/hylstre_maatte.jpg")
+img_grayscale = cv2.imread("images/hylstre_maatte.jpg", 0) #Convert to grayscale
 
-cv2.namedWindow("Grayscale", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Grayscale", imgResizeX, imgResizeY)
-cv2.imshow("Grayscale", grayscale)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def img_plot():
+    plot.subplot(121)
+    cv2.namedWindow("Grayscale", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Grayscale", imgResizeX, imgResizeY)
+    cv2.imshow("Grayscale", img_grayscale)
 
-grayscale_histogram = cv2.calcHist([grayscale],[0],None,[256],[0,256]) #Create histogram of grayscale img
+    plot.subplot(122)
+    cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Original", imgResizeX, imgResizeY)
+    cv2.imshow("Original", img_original)
 
-plot.subplot(221), plot.imshow(grayscale, cmap="Grays")
-plot.subplot(222), plot.plot(grayscale_histogram)
-plot.xlim(0, 256)
+    plot.tight_layout()
+    plot.show()
 
-plot.show()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def compare_histograms():
+    grayscale_histogram = cv2.calcHist([img_grayscale], [0], None, [256], [0,256]) # Create histogram of grayscale img
+
+    # Create equalized grayscale image and histogram
+    grayscale_BGR2GRAY = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
+    equalized_img = cv2.equalizeHist(grayscale_BGR2GRAY)
+    equalized_histogram = cv2.calcHist([equalized_img], [0], None, [256], [0,256])
+
+    # Plot images and histograms
+    plot.subplot(221)
+    plot.imshow(img_grayscale, cmap='gray')
+    plot.title('Original Grayscale')
+    plot.axis('off')
+
+    plot.subplot(222)
+    plot.plot(grayscale_histogram, color='k')
+    plot.title('Histogram Original')
+    plot.xlim([0,256])
+
+    plot.subplot(223)
+    plot.imshow(equalized_img, cmap='gray')
+    plot.title('Equalized Grayscale')
+    plot.axis('off')
+
+    plot.subplot(224)
+    plot.plot(equalized_histogram, color='k')
+    plot.title('Histogram Equalized')
+    plot.xlim([0,256])
+
+    plot.tight_layout()
+    plot.show()
+
+img_plot()
+compare_histograms()
