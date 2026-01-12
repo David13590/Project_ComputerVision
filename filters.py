520dec60511a023cd4170da99ecc87633dae0a95
dflindt <df@dflindtatflindt.com>
2026-01-12 11:23:50 +0100

    Correct contour count on normal histogram

--- filters.py
+++ filters.py
@@ -34,8 +34,8 @@ def compare_histograms():
 
     # Create equalized grayscale image and histogram
     grayscale_BGR2GRAY = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
-    equalized_img = cv2.equalizeHist(grayscale_BGR2GRAY)
-    equalized_histogram = cv2.calcHist([equalized_img], [0], None, [256], [0,256])
+    img_equalized = cv2.equalizeHist(grayscale_BGR2GRAY)
+    equalized_histogram = cv2.calcHist([img_equalized], [0], None, [256], [0,256])
 
     # Plot images and histograms
     plot.figure(2) # Create and activate figure 2
@@ -51,7 +51,7 @@ def compare_histograms():
     plot.xlim([0,256])
 
     plot.subplot(223)
-    plot.imshow(equalized_img, cmap='gray')
+    plot.imshow(img_equalized, cmap='gray')
     plot.title('Equalized Grayscale')
     plot.axis('off')
 
@@ -61,10 +61,60 @@ def compare_histograms():
     plot.xlim([0,256])
 
     plot.tight_layout()
+
+    return img_grayscale, img_equalized
+
+# Contour detect 
+def contour_detect(img, img2):
+    thresh_lower_bound = 200
+    thresh_upper_bound = 255
+    thresh_type = [cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV, cv2.THRESH_TRUNC, cv2.THRESH_TOZERO, cv2.THRESH_TOZERO_INV]
+
+    contour_line_thickness = 2
+    contour_parrent_idx = -1 # -1 to draw all contours
+    contour_line_color = (0, 255, 0) # BGR
+    contour_min_area = 200  # Minimum area to consider a contour
+
+    # First img
+    ret, thresh = cv2.threshold(img, thresh_lower_bound, thresh_upper_bound, thresh_type[0]) # Apply threshold for better accuracy
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours in thresholded image
+    contour_filtered = [c for c in contours if cv2.contourArea(c) >= contour_min_area]  # Filter contours by area
+
+    # Draw contours on a copy of the original image
+    img_contours = img.copy()
+    cv2.drawContours(img_contours, contour_filtered, contour_parrent_idx, contour_line_color, contour_line_thickness) # Draw all contours
     
+    # Second img
+    ret2, thresh2 = cv2.threshold(img2, thresh_lower_bound, thresh_upper_bound, thresh_type[0]) # Apply threshold for better accuracy
+    contours2, hierarchy2 = cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours in thresholded image 
+    contour2_filtered = [c for c in contours2 if cv2.contourArea(c) >= contour_min_area]  # Filter contours by area
+
+    img2_contours = img2.copy()
+    cv2.drawContours(img2_contours, contour2_filtered, contour_parrent_idx, contour_line_color, contour_line_thickness) # Draw all contours
+
+    # Plot contours
+    plot.figure(3) # Create and activate figure 3
+
+    plot.subplot(211)
+    plot.imshow(img_contours, cmap='gray')
+    plot.text(0.00, 0.95, f"count: {len(contour_filtered)}", transform=plot.gca().transAxes, fontsize=9, color='green') # Add contour count text
+    plot.title('Contours')
+    plot.axis('off')
+
+    plot.subplot(212)
+    plot.imshow(img2_contours, cmap='gray')
+    plot.text(0.00, 0.95, f"count: {len(contour2_filtered)}", transform=plot.gca().transAxes, fontsize=9, color='green') # Add contour count text
+    plot.title('Contours Equalized')
+    plot.axis('off')
+    
+    return contours, img_contours, contours2, img2_contours
+
+
+
 # Call plot functions
 img_plot() 
-compare_histograms()
+img_grayscale_hist, img_equalized_hist = compare_histograms() # Save returned values
+contours_grayscale = contour_detect(img_grayscale_hist, img_equalized_hist)
 
 plot.show() # Show all plots
 cv2.waitKey(0)
